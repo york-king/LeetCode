87a11cbed0b5080deb90f5b2e24854763a5b7dd6
york-king <972850866@qq.com>
2021-10-26 15:12:37 +0800

    feat: 435.无重叠区间 新增注释

--- 435.无重叠区间.cpp
+++ 435.无重叠区间.cpp
@@ -14,13 +14,15 @@ public:
             return 0;
         }
         
+        //lambda表达式 以区间的较大值从小到大 排序
         sort(intervals.begin(), intervals.end(), [] (vector<int> a, vector<int> b) { return a[1] < b[1]; } );
 
+        //前一个区间的最大值
         int pre = intervals[0][1];
-
         int res = 0;
         for (int i = 1; i < num; i++)
         {
+            //前一个区间最大值与现在区间有重合 则加1
             if (pre > intervals[i][0])
             {
                 res++;
